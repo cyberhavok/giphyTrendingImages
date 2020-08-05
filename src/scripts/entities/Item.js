e8e0461f188088296ac9976fa91f3e771d6a1a72
= <=>
2020-08-05 00:06:16 -0500

    Entity for Image Html element

---
+++ src/scripts/entities/Item.js
@@ -0,0 +1,24 @@
+class Item {
+    constructor(url, classNameStyle) {
+        this._url = url;
+        this._classNameStyle = classNameStyle;
+    }
+
+    get url() {
+        return this._url;
+    }
+
+    get classNameStyle() {
+        return this._classNameStyle;
+    }
+
+    set url(url) {
+        this._url = url;
+    }
+
+    set classNameStyle(classNameStyle) {
+        this._classNameStyle = classNameStyle;
+    }
+}
+
+export { Item };
